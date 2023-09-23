fad05b127ae53c32f12745c8f65d8181fca99df9
Amine Rajma <rajmaamine@gmail.com>
2023-09-23 09:48:24 -0700

    just-commiting

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program using printf to print
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+		printf("with proper grammar, but outcome is a piece of art,\n");
+			return (0);
+}
