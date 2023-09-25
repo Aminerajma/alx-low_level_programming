ff0543f097e58e12df3b5b46b1c6a3b9bd7fea76
Amine Rajma <rajmaamine@gmail.com>
2023-09-25 13:03:12 -0700

    just-commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print single digits starting with 0 using putchar
+ *
+ *Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+}
